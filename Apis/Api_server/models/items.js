d487252111af75176fb515f040c9cdd59b7b1caa
Amit12895 <amit.mani128@gmail.com>
2021-04-05 22:40:58 -0400

    menu api

---
+++ Apis/Api_server/models/items.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var itemsSchema = new mongoose.Schema({
+    cat_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'restaurant_cat',
+            required: true,
+        },
+    res_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'restaurant_list',
+            required: true,
+           
+        },
+    item_name: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+    });
+    mongoose.model('items', itemsSchema, 'items');
+    
