78ebd545e10f3b5e19eed2393bfe46e33e378243
Aaron <mac@Aaronss-MacBook-Pro.local>
2021-04-20 19:44:10 -0400

    navbar bug fix

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -68,7 +68,7 @@ export default class Navbar extends Component {
     render() {
         
         
-        if(localStorage.getItem('token')){
+        if(localStorage.getItem('token')!=null || localStorage.getItem('token')!==''){
             
             
             return(
@@ -105,10 +105,10 @@ const NavBarSignIn= ({toggle})=>{
                         </NavItem>
                         <NavItem>
                             <NavLinks to="/cart">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="24" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="24" fill="currentColor" className="bi bi-cart4" viewBox="0 0 16 16">
                                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
-</svg><span id="counter" class="cart-basket d-flex align-items-center justify-content-center">
-            {JSON.parse(localStorage.getItem('cart')).length!=null?JSON.parse(localStorage.getItem('cart')).length:0}
+</svg><span id="counter" className="cart-basket d-flex align-items-center justify-content-center">
+            {JSON.parse(localStorage.getItem('cart'))!=null?JSON.parse(localStorage.getItem('cart')).length:0}
           </span>
 </NavLinks>
 
@@ -117,7 +117,7 @@ const NavBarSignIn= ({toggle})=>{
                     </NavMenu>
                              
                   <NavBtn>
-                      <NavBtnLink  >Sign out</NavBtnLink>
+                      <NavBtnLink to= "/">Sign out</NavBtnLink>
                   </NavBtn>
 
 
@@ -139,32 +139,22 @@ const NavBarSignOut= ({toggle})=>{
                     </MobileIcon  > 
                     <NavMenu>
                         <NavItem>
-                            <NavLinks to="about"> About</NavLinks>
+                            <NavLinks to="/about"> About</NavLinks>
                         </NavItem>
                         <NavItem>
-                            <NavLinks to="list">Restaurants </NavLinks>
+                            <NavLinks to="/list">Restaurants </NavLinks>
                         </NavItem>
                         <NavItem>
                             <NavLinks to="community"> Community</NavLinks>
                         </NavItem>
+                        
                         <NavItem>
-                            <NavLinks to="/cart">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="24" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
-                                   <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
-</svg><span id="counter" class="cart-basket d-flex align-items-center justify-content-center">
-{JSON.parse(localStorage.getItem('cart')).length}
-
-          </span>
-</NavLinks>
-
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="Sign Up"> Sign up</NavLinks>
+                            <NavLinks to="/signin"> Sign up</NavLinks>
                         </NavItem>
                     </NavMenu>
                              
                     <NavBtn>
-                      <NavBtnLink to="signin">Sign In</NavBtnLink>
+                      <NavBtnLink to="/signin">Sign In</NavBtnLink>
                  </NavBtn>
 
                 </NavbarContainer>
